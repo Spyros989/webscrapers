72c78f1b4c6d60f935c19defe779dc91bcf62bde
Spyros989 <spyrosfofolos@gmail.com>
2026-06-03 00:22:03 +0200

    test

--- bands_fb_scrapers/ma_bands_fb_scrapers/ma_bands_fb_scrapers_ad_2026.py
+++ bands_fb_scrapers/ma_bands_fb_scrapers/ma_bands_fb_scrapers_ad_2026.py
@@ -8,15 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime
 from pathlib import Path
 import time
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # ----------------------------
 # CONFIG
 # ----------------------------
-DB_USER = "postgres"
-DB_PASS = "2102232094"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-DB_NAME = "homelab_db"
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME")
 
 engine = create_engine(
     f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
